47b6b5f1b018d9e918f67c5a7d9e6a9f78f1bd71
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-12-02 15:47:30 +0100

    bug-array-vuote

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,45 @@
 //importo le due funzioni di GlobalContext.jsx
-import { GlobalContextProvider, useGlobalContext, MovieList } from './context/GlobalContext'
+import {
+  GlobalContextProvider,
+  useGlobalContext,
+  MovieList,
+} from './context/GlobalContext';
 //css
-import './App.css'
+import './App.css';
 
 function App() {
-
   //funzione AppHeader con le costanti necessarie per far funzionare il form
   function AppHeader() {
-    const { searchText, setSearchText, base_movies_api_url } = useGlobalContext()
-    console.log(base_movies_api_url);
+    const { searchText, setSearchText, HandleSearchTextSubmit } =
+      useGlobalContext();
 
     return (
       <header>
         <div className="logo">Boolflix</div>
         {/* form */}
         <form onSubmit={HandleSearchTextSubmit}>
-          <input type="text" placeholder='Search a film' onChange={(e) => setSearchText(e.target.value)} value={searchText} />
+          <input
+            type="text"
+            placeholder="Search a film"
+            onChange={(e) => setSearchText(e.target.value)}
+            value={searchText}
+          />
         </form>
       </header>
-    )
+    );
   }
-
 
   return (
     <>
       <GlobalContextProvider>
-
         <AppHeader />
 
         <main>
           <MovieList />
         </main>
-
       </GlobalContextProvider>
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,75 +1,73 @@
 //context
-import { createContext, useContext } from "react";
+import { createContext, useContext } from 'react';
 const GlobalContext = createContext();
 
 //useState
-import { useState } from "react";
+import { useState } from 'react';
 
 //funzione che racchiude costanti e logica per:
 //1-accedere all'api_key
 //2-accedere all'url
 //3-accedere all'array di film richiamando l'url e aggiornando "movies"
-//4-accedere al valore scritto nell'imput tramite le funzioni scritte nell'imput in App.jsx, aggiornando "searchText" per la ricerca dei film 
+//4-accedere al valore scritto nell'imput tramite le funzioni scritte nell'imput in App.jsx, aggiornando "searchText" per la ricerca dei film
 function GlobalContextProvider({ children }) {
     const api_key = import.meta.env.VITE_MOVIE_DB_API_KEY;
-    const base_movies_api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchText}`;
+    const [movies, setMovies] = useState([]);
+    const [searchText, setSearchText] = useState('');
 
-    const [movies, setMovies] = useState([])
-    const [searchText, setSearchText] = useState("")
+    // Calcola dinamicamente l'URL dell'API
+    const base_movies_api_url = `https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=${searchText}`;
 
-    //funzione per il form, al submit stampa i film corrispondenti al valore scritto in input
     function HandleSearchTextSubmit(e) {
-        e.preventDefault()
+        e.preventDefault();
         console.log(base_movies_api_url);
 
-        //chiamata api che aggiorna il valore di movies
+        // Chiamata API per aggiornare "movies"
         fetch(base_movies_api_url)
             .then((res) => res.json())
             .then(({ results }) => {
                 console.log(results);
-                setMovies(results)
-            })
+                setMovies(results);
+            });
     }
 
-
-
-    //costante con costanti che devono essere accessibili al children tramite <GlobalContext.Provider value={values}>
     const values = {
         movies,
         setMovies,
         searchText,
         setSearchText,
-        base_movies_api_url,
+        base_movies_api_url, // Non è necessario usarlo direttamente, ma lo lasciamo per eventuali utilizzi futuri
         HandleSearchTextSubmit,
-        MovieList
+        MovieList,
     };
 
     return (
-        //questo return darà accesso ai figli di GlobalContext.Provider a tutti i valori "values"
-        <GlobalContext.Provider value={values}>
-            {children}
-        </GlobalContext.Provider>
-    )
+        <GlobalContext.Provider value={values}>{children}</GlobalContext.Provider>
+    );
 }
 
 function MovieList() {
     //non ho ben chiara questa costante
-    const { movies } = useGlobalContext()
+    const { movies } = useGlobalContext();
     console.log(movies);
 
     return (
         //markup e map per ciclare nell'array di film
         <ul className="movie-list">
-            {movies && movies.map((movie, index) => (
-                <li key={index} style={{ borderBottom: "2px solid black", marginBottom: "1rem" }}>
-                    {movie.title} <br />
-                    {movie.original_title} <br />
-                    {movie.original_lenguage} <br />
-                    {movie.vote_average} <br />
-                </li>
-            ))}
+            {movies &&
+                movies.map((movie, index) => (
+                    <li
+                        key={index}
+                        style={{ borderBottom: '2px solid black', marginBottom: '1rem' }}
+                    >
+                        {movie.title} <br />
+                        {movie.original_title} <br />
+                        {movie.original_lenguage} <br />
+                        {movie.vote_average} <br />
+                    </li>
+                ))}
         </ul>
-    )
+    );
 }
 
 //non ho ben chiara questa funzione
@@ -79,4 +77,13 @@ function useGlobalContext() {
 
 //esporto funzioni
 export { GlobalContextProvider, useGlobalContext, MovieList };
+
+
+
+
+
+
+
+/*     const base_movies_api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchText}`;
+ */
 
